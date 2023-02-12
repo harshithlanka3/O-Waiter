c5b211460c03a120fe6f20671867ba522a0767af
Paige Holland <pholland7@gatech.edu>
2023-02-12 18:51:01 -0500

    Added ability to read progress bar     - Make sure to remove from error message

--- app/src/main/java/com/example/roadjump/ConfigActivity.java
+++ app/src/main/java/com/example/roadjump/ConfigActivity.java
@@ -7,6 +7,7 @@ import android.widget.EditText;
 import android.widget.Button;
 import android.content.Intent;
 import android.widget.ImageButton;
+import android.widget.SeekBar;
 
 public class ConfigActivity extends AppCompatActivity {
     private Button startJump;
@@ -15,6 +16,8 @@ public class ConfigActivity extends AppCompatActivity {
     private ImageButton bunnyButton;
     private ImageButton pepeButton;
     private EditText userName;
+
+    private SeekBar difficultyBar;
 
     private boolean userValid;
 
@@ -28,6 +31,7 @@ public class ConfigActivity extends AppCompatActivity {
         bunnyButton = findViewById(R.id.bunny_sprite);
         pepeButton = findViewById(R.id.pepe_sprite);
         userName = findViewById(R.id.username);
+        difficultyBar = findViewById(R.id.difficultyBar);
 
         startJump.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,7 +43,7 @@ public class ConfigActivity extends AppCompatActivity {
 //                    startActivity(playGame);
                 } else {
                     userName.setError("You have to enter a valid username."
-                            + "Can't be null or whitespace.");
+                            + "Can't be null or whitespace." + difficultyBar.getProgress());
                 }
             }
 
@@ -52,4 +56,5 @@ public class ConfigActivity extends AppCompatActivity {
             return userName.getText().toString().trim().length() != 0;
         }
     }
+
 }
