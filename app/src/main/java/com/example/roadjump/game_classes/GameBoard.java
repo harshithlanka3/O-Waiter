9563ee43fe6918f0e463e9ff9dc903ce79d2e51c
Harshith Lanka <hlanka3@gatech.edu>
2023-02-20 14:28:07 -0500

    Added game board functionality

--- app/src/main/java/com/example/roadjump/game_classes/GameBoard.java
+++ app/src/main/java/com/example/roadjump/game_classes/GameBoard.java
@@ -4,6 +4,14 @@ public class GameBoard {
     private Tile[][] board;
 
     public GameBoard() {
-        board = new Tile[][]
+        board = new Tile[9][9];
+    }
+
+    public void addTile(Tile tile , int row, int column) {
+        board[row][column] = tile;
+    }
+
+    public Tile getTile(int row, int column) {
+        return board[row][column];
     }
 }
