1d6510dc665c45084376e0e38e7bb4cf4ba3ebea
Ramya Subramaniam <ramyasub@gatech.edu>
2023-02-20 15:18:24 -0500

    Initialize Floor Tile and SushiBeltTile

--- app/src/main/java/com/example/roadjump/game_classes/FloorTile.java
+++ app/src/main/java/com/example/roadjump/game_classes/FloorTile.java
@@ -1,4 +1,12 @@
 package com.example.roadjump.game_classes;
 
-public class FloorTile {
+public class FloorTile extends Tile {
+    private String imageId;
+
+    private boolean isSafe = true;
+
+    public boolean getIsSafe() {
+        return isSafe;
+    }
+
 }
