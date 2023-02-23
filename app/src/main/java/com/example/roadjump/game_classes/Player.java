10a46c964ca7c0925b07599e903a11bbef98615c
Giselle McPhilliamy <gmcphilliamy3@gatech.edu>
2023-02-23 13:32:21 -0500

    player class creation and construction creation

--- app/src/main/java/com/example/roadjump/game_classes/Player.java
+++ app/src/main/java/com/example/roadjump/game_classes/Player.java
@@ -1,8 +1,45 @@
 package com.example.roadjump.game_classes;
 
+import android.widget.ImageView;
+
+import com.example.roadjump.R;
+
 public class Player {
     private float xCoord;
     private float yCoord;
-
+    private ImageView characterImage;
     private float score;
+
+    public Player(ImageView characterImage) {
+        this.characterImage = characterImage;
+        xCoord = 0;
+        yCoord = 0;
+        score = 0;
+    }
+
+    public float getxCoord() {
+        return xCoord;
+    }
+    public float getyCoord() {
+        return yCoord;
+    }
+    public float getScore() {
+        return score;
+    }
+    public ImageView getCharacterImage() {
+        return characterImage;
+    }
+
+    public void setxCoord(float x) {
+        xCoord = x;
+    }
+    public void setyCoord(float y) {
+        xCoord = y;
+    }
+    public void setScore(float score) {
+        this.score = score;
+    }
+    public void setCharacterImage(ImageView imageChar) {
+        characterImage = imageChar;
+    }
 }
