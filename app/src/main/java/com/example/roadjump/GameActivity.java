75304ebc87dcf50404373343e38ce87b37731283
Giselle McPhilliamy <gmcphilliamy3@gatech.edu>
2023-02-23 11:54:42 -0500

    update to the font

--- app/src/main/java/com/example/roadjump/GameActivity.java
+++ app/src/main/java/com/example/roadjump/GameActivity.java
@@ -2,12 +2,16 @@ package com.example.roadjump;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.content.Intent;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.animation.ObjectAnimator;
+
 
 public class GameActivity extends AppCompatActivity {
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
@@ -18,6 +22,8 @@ public class GameActivity extends AppCompatActivity {
         int characterNum = intent.getIntExtra("character", 0);
         int difficultyNum = intent.getIntExtra("difficulty", 0);
         String inputUsername = intent.getExtras().getString("user");
+
+        //ImageView backgroundImage = findViewById(R.id.characterSpriteImage);
 
         ImageView characterImage = findViewById(R.id.characterSpriteImage);
         TextView livesText = findViewById(R.id.livesNumTxt);
@@ -34,6 +40,20 @@ public class GameActivity extends AppCompatActivity {
             characterImage.setImageResource(R.drawable.bunny);
             break;
         }
+        //animating object movement
+        /*
+        AnimationDrawable characterAnimation = (AnimationDrawable) characterImage.getBackground();
+
+        backImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                rocketAnimation.start();
+            }
+        //Move object up 32 pixles;
+        ObjectAnimator animation = ObjectAnimator.ofFloat(characterImage, "translationY", 32f);
+        animation.setDuration(2000);
+        animation.start();
+        */
 
         switch (difficultyNum) {
         case 1:
@@ -48,5 +68,8 @@ public class GameActivity extends AppCompatActivity {
         }
 
         userText.setText(inputUsername);
+
+        //making the character move
+
     }
 }
