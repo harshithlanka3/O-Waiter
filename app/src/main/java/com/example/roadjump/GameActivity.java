a7510ef17acb8af46c7fca9c5fd9846e00270c51
Giselle McPhilliamy <gmcphilliamy3@gatech.edu>
2023-02-24 18:18:26 -0500

    movement button code

--- app/src/main/java/com/example/roadjump/GameActivity.java
+++ app/src/main/java/com/example/roadjump/GameActivity.java
@@ -1,6 +1,8 @@
 package com.example.roadjump;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintSet;
+import androidx.constraintlayout.widget.*;
 
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
@@ -8,6 +10,7 @@ import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.animation.ObjectAnimator;
 
@@ -73,22 +76,38 @@ public class GameActivity extends AppCompatActivity {
 
         // registering button
         upBtn = findViewById(R.id.upBtn);
+        ConstraintLayout.LayoutParams layoutParams = (ConstraintLayout.LayoutParams) characterImage.getLayoutParams();
 
         //handling the start button
         upBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 currentGamePlayer.setyCoord(currentGamePlayer.getyCoord() + 1);
+
+                layoutParams.topMargin = layoutParams.topMargin - 10;
+                //layoutParams.bottomMargin = (int) currentGamePlayer.getyCoord();//your bottom margin value
+                characterImage.setLayoutParams(layoutParams);
             }
         });
 
         downBtn = findViewById(R.id.downBtn);
 
+
+        //Constraints.LayoutParams layoutParams = (Constraints.LayoutParams) characterImage.getLayoutParams();
+        //RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) characterImage.getLayoutParams();
+
         //handling the start button
         downBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                currentGamePlayer.setyCoord(currentGamePlayer.getyCoord() - 1);
+                currentGamePlayer.setyCoord(currentGamePlayer.getyCoord() - 10);
+                layoutParams.bottomMargin = layoutParams.bottomMargin - 10;
+                //layoutParams.bottomMargin = (int) currentGamePlayer.getyCoord();//your bottom margin value
+                characterImage.setLayoutParams(layoutParams);
+
+                //Constraints.LayoutParams lp= (ConstraintLayout.LayoutParams) characterImage.getLayoutParams();
+                //lp.setMargins(left, top, right, bottom);
+                //list.setLayoutParams(lp);
             }
         });
 
@@ -98,7 +117,11 @@ public class GameActivity extends AppCompatActivity {
         leftBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                currentGamePlayer.setxCoord(currentGamePlayer.getxCoord() - 1);
+                currentGamePlayer.setxCoord(currentGamePlayer.getxCoord() - 10);
+
+                layoutParams.leftMargin = layoutParams.leftMargin - 10;
+                //layoutParams.bottomMargin = (int) currentGamePlayer.getyCoord();//your bottom margin value
+                characterImage.setLayoutParams(layoutParams);
             }
         });
 
@@ -108,6 +131,10 @@ public class GameActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 currentGamePlayer.setxCoord(currentGamePlayer.getxCoord() + 1);
+
+                layoutParams.rightMargin = layoutParams.rightMargin - 10;
+                //layoutParams.bottomMargin = (int) currentGamePlayer.getyCoord();//your bottom margin value
+                characterImage.setLayoutParams(layoutParams);
             }
         });
     }
