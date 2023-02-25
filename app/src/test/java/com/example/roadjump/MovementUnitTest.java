d59cfbf641af617a8d4d4d4c20e564d84e40a4f0
Sean Spych <sspych3@gatech.edu>
2023-02-25 16:18:09 -0500

    Implementing movement tests in MovementUnitTest

--- app/src/test/java/com/example/roadjump/MovementUnitTest.java
+++ app/src/test/java/com/example/roadjump/MovementUnitTest.java
@@ -21,28 +21,61 @@ public class MovementUnitTest {
     @Test
     public void movingForwardTest() {
         bruh.moveUp();
+
+        assertEquals(600, bruh.getxCoord(), 0);
+        assertEquals(512, bruh.getyCoord(), 0);
+
         bruh.moveUp();
         bruh.moveUp();
 
-        assertEquals(bruh.getxCoord(),  600);
-        assertEquals(bruh.getyCoord(), 336);
+        assertEquals(600,  bruh.getxCoord(), 0);
+        assertEquals(336, bruh.getyCoord(), 0);
     }
 
     //test2: moving backwards
     @Test
     public void movingBackwardsTest() {
+        bruh.moveDown();
 
+        assertEquals(600, bruh.getxCoord(), 0);
+        assertEquals(688, bruh.getyCoord(), 0);
+
+        bruh.moveDown();
+        bruh.moveDown();
+
+        assertEquals(600, bruh.getxCoord(), 0);
+        assertEquals(864, bruh.getyCoord(), 0);
     }
 
     //test3: moving right
     @Test
     public void movingRightTest() {
+        bruh.moveRight();
+
+        assertEquals(512, bruh.getxCoord(), 0);
+        assertEquals(600, bruh.getyCoord(), 0);
+
+        bruh.moveRight();
+        bruh.moveRight();
+
+        assertEquals(336, bruh.getxCoord(), 0);
+        assertEquals(600, bruh.getyCoord(), 0);
 
     }
+
     //test4: moving left
     @Test
     public void movingLeftTest() {
+        bruh.moveLeft();
 
+        assertEquals(688, bruh.getxCoord(), 0);
+        assertEquals(600, bruh.getyCoord(), 0);
+
+        bruh.moveLeft();
+        bruh.moveLeft();
+
+        assertEquals(864, bruh.getxCoord(), 0);
+        assertEquals(600, bruh.getyCoord(), 0);
     }
 
 
