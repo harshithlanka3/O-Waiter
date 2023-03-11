c6a98b9f1feab9ffdc237b773fddd2064547e8a6
Ramya Subramaniam <ramyasub@gatech.edu>
2023-03-11 12:50:31 -0500

    Constructors for the vehicle

--- app/src/main/java/com/example/roadjump/Vehicle.java
+++ app/src/main/java/com/example/roadjump/Vehicle.java
@@ -6,5 +6,23 @@ public class Vehicle {
     private final int lowerBound = 1754;
     private final int leftBound = 12;
     private final int rightBound = 1068;
+    int widthSprite;
+    int heightSprite;
+    int delay;
+
+    //1 constructor
+
+    public Vehicle (int delay, int width, int height, int xCoord, int yCoord) {
+        this.delay = delay;
+        this.widthSprite = width;
+        this.heightSprite = height;
+        this.xCoord = xCoord;
+        this.yCoord = yCoord;
+    }
+    public Vehicle (int delay, int width, int height) {
+        this(delay, width, height, 0, 0);
+    }
+
+
 
 }
